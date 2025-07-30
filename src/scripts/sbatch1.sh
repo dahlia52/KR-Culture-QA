8825976d5f8667cca0a3e2e0e484cff8b505744f
dahlia52 <wjdwlgp2001@ewhain.net>
2025-07-30 07:37:04 +0000

    [250730]-2

---
+++ src/scripts/sbatch1.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+ 
+#SBATCH -J inference_verify         
+#SBATCH -o /home/wisdomjeong/KR-Culture-QA/log/inference_verify-%j.out   
+#SBATCH -p RTX4090         
+#SBATCH -t 3-00:00:00            
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBTACH --ntasks=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=6
+
+# 환경 설정
+echo "Activate conda"
+source /home/wisdomjeong/miniconda3/bin/activate kcqa
+cd /home/wisdomjeong/KR-Culture-QA
+pwd
+
+echo "Start Running"
+
+# 실행 명령어
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+python src/inference_transformers.py --batch_size 1 --output "./resource/QA/final.json"
