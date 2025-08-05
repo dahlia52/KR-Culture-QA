1233009bdccb0136e1ccdc576bcf2ad8738fa089
Jihae Jeong <101353482+dahlia52@users.noreply.github.com>
2025-08-06 00:48:05 +0900

    Delete scripts/test2.sh

--- scripts/test2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Start Inference"
-
-# 실행 명령어
-python -m run.test --model_id "jjae/Midm-KCulture-2.0-Base-Instruct" --input "./resource/QA/korean_culture_qa_V1.0_test+.json" --output "./resource/QA/result_0.01.json" --quantize --temperature 0.01 --device "cuda:6"
